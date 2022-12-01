43fac52c9fbc786199e5b7c5bff4870ae948cd50
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-12-01 13:00:12 +0000

    Minor reformat

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-pub fn parse(input: String) -> Vec<Vec<u16>> {
+pub fn parse(_input: String) -> Vec<Vec<u16>> {
     vec!()
 }
 
@@ -8,27 +8,19 @@ mod tests {
 
     #[test]
     fn it_parses() {
-        let input = "1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000";
+        let input = format!("{}{}{}{}{}",
+                    "1000\n2000\n3000\n",
+                    "4000\n\n",
+                    "5000\n6000\n\n",
+                    "7000\n8000\n9000\n\n",
+                    "10000\n");
         let expected = vec!(
             vec!(1000, 2000, 3000),
             vec!(4000),
             vec!(5000, 6000),
             vec!(7000, 8000, 9000),
             vec!(10000));
-        assert_eq!(parse(String::from(input)), expected);
+        assert_eq!(parse(input), expected);
     }
 }
 
