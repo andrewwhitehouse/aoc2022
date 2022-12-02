0d4372d8117eff2bbf1f8fcfcaa6c4c8679d48ed
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-12-02 18:59:30 +0000

    Reorganise for day 2

---
+++ src/day1.rs
@@ -0,0 +1,78 @@
+pub fn parse(input: String) -> Vec<Vec<u32>> {
+    let mut batches = Vec::new();
+    let mut current: Vec<u32> = Vec::new();
+    for line in input.trim_end().split("\n") {
+        if line.trim().len() == 0 {
+            if current.len() > 0 {
+                batches.push(current);
+                current = Vec::new();
+            }
+        } else {
+            current.push(line.parse::<u32>().unwrap());
+        }
+    }
+    if current.len() > 0 {
+        batches.push(current);
+    }
+    batches
+}
+
+pub fn max_sum(batches: Vec<Vec<u32>>) -> u32 {
+    let mut max = 0u32;
+    for batch in batches {
+        let batch_sum = batch.iter().sum();
+        if batch_sum > max {
+            max = batch_sum;
+        }
+    }
+    max
+}
+
+pub fn sum_of_top_three(batches: Vec<Vec<u32>>) -> u32 {
+    let mut sums: Vec<u32> = batches.iter().map(|batch| batch.iter().sum()).collect();
+    sums.sort_by(|a, b| b.cmp(a));
+    sums[0] + sums[1] + sums[2]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_parses() {
+        let input = format!(
+            "{}{}{}{}{}",
+            "1000\n2000\n3000\n\n", "4000\n\n", "5000\n6000\n\n", "7000\n8000\n9000\n\n", "10000\n"
+        );
+        let expected = vec![
+            vec![1000, 2000, 3000],
+            vec![4000],
+            vec![5000, 6000],
+            vec![7000, 8000, 9000],
+            vec![10000],
+        ];
+        assert_eq!(parse(input), expected);
+    }
+
+    #[test]
+    fn find_max_sum() {
+        let batches = vec!(
+            vec!(1000, 2000, 3000),
+            vec!(4000),
+            vec!(5000, 6000),
+            vec!(7000, 8000, 9000),
+            vec!(10000));
+        assert_eq!(max_sum(batches), 24_000);
+    }
+
+    #[test]
+    fn find_sum_of_top_three() {
+        let batches = vec!(
+            vec!(1000, 2000, 3000),
+            vec!(4000),
+            vec!(5000, 6000),
+            vec!(7000, 8000, 9000),
+            vec!(10000));
+        assert_eq!(sum_of_top_three(batches), 45_000);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,14 @@
 use std::fs;
 
-pub fn parse(input: String) -> Vec<Vec<u32>> {
-    let mut batches = Vec::new();
-    let mut current: Vec<u32> = Vec::new();
-    for line in input.trim_end().split("\n") {
-        if line.trim().len() == 0 {
-            if current.len() > 0 {
-                batches.push(current);
-                current = Vec::new();
-            }
-        } else {
-            current.push(line.parse::<u32>().unwrap());
-        }
-    }
-    if current.len() > 0 {
-        batches.push(current);
-    }
-    batches
-}
+mod day1;
 
-pub fn max_sum(batches: Vec<Vec<u32>>) -> u32 {
-    let mut max = 0u32;
-    for batch in batches {
-        let batch_sum = batch.iter().sum();
-        if batch_sum > max {
-            max = batch_sum;
-        }
-    }
-    max
-}
-
-pub fn sum_of_top_three(batches: Vec<Vec<u32>>) -> u32 {
-    let mut sums: Vec<u32> = batches.iter().map(|batch| batch.iter().sum()).collect();
-    sums.sort_by(|a, b| b.cmp(a));
-    sums[0] + sums[1] + sums[2]
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_parses() {
-        let input = format!(
-            "{}{}{}{}{}",
-            "1000\n2000\n3000\n\n", "4000\n\n", "5000\n6000\n\n", "7000\n8000\n9000\n\n", "10000\n"
-        );
-        let expected = vec![
-            vec![1000, 2000, 3000],
-            vec![4000],
-            vec![5000, 6000],
-            vec![7000, 8000, 9000],
-            vec![10000],
-        ];
-        assert_eq!(parse(input), expected);
-    }
-
-    #[test]
-    fn find_max_sum() {
-        let batches = vec!(
-            vec!(1000, 2000, 3000),
-            vec!(4000),
-            vec!(5000, 6000),
-            vec!(7000, 8000, 9000),
-            vec!(10000));
-        assert_eq!(max_sum(batches), 24_000);
-    }
-
-    #[test]
-    fn find_sum_of_top_three() {
-        let batches = vec!(
-            vec!(1000, 2000, 3000),
-            vec!(4000),
-            vec!(5000, 6000),
-            vec!(7000, 8000, 9000),
-            vec!(10000));
-        assert_eq!(sum_of_top_three(batches), 45_000);
-    }
-}
-
-fn main() {
+fn solve_day1() {
     let input = fs::read_to_string("input/day1.txt").expect("failed to read day1 input");
-    let batches = parse(input);
-    println!("Day 1 Part 1 {}", max_sum(batches.clone()));
-    println!("Day 1 Part 2 {}", sum_of_top_three(batches));
+    let batches = day1::parse(input);
+    println!("Day 1 Part 1 {}", day1::max_sum(batches.clone()));
+    println!("Day 1 Part 2 {}", day1::sum_of_top_three(batches));
+}
+
+pub fn main() {
+    solve_day1();
 }
