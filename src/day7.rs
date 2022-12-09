cf222539d9f1b9e204073259d2623916eec6472b
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-12-09 16:28:40 +0000

    Work in progress

--- src/day7.rs
+++ src/day7.rs
@@ -1,22 +1,55 @@
+use std::collections::HashMap;
+
 #[derive(PartialEq, Debug)]
 pub struct File {
     name: String,
     size: u32,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub struct Directory {
     name: String,
-    files: Vec<File>,
-    sub_directories: Vec<Directory>,
+    files: HashMap<String, u32>,
+    sub_directories: HashMap<String, Directory>,
+    parent: Vec<Directory> // I can't think of another way to do this yet
 }
 
 pub fn parse(input: String) -> Directory {
-    Directory {
+    let locaion = "/"; // Assume we start from /
+    let root = Directory {
         name: String::from("/"),
-        files: vec![],
-        sub_directories: vec![],
+        files: HashMap::new(),
+        sub_directories: HashMap::new(),
+        parent: Vec::new()
+    };
+    let mut current = root;
+    for line in input.trim().split("\n") {
+        if line.starts_with('$') {
+            let parts: Vec<&str> = line.split(" ").collect();
+            if parts[1] == "cd" {
+                if parts[2] == ".." {
+                    current = current.parent[0];
+                } else {
+                    let target = current.sub_directories.get(parts[2]).unwrap();
+                    current = *target;
+                }
+            }
+        } else {
+            let mut directories = Vec::new();
+            let mut files = Vec::new();
+            let parts: Vec<&str> = line.split(" ").collect();
+            if parts[0] == "dir" {
+                current.sub_directories.insert(parts[1].to_string(), Directory { name: parts[1].to_string(),
+                    files: HashMap::new(),
+                    sub_directories: HashMap::new(),
+                    parent: vec!(current.clone())});
+            } else {
+                let size = parts[0].parse::<u32>().unwrap;
+                current.files.insert(parts[1].to_string(), size);
+            }
+        }
     }
+    root
 }
 
 #[cfg(test)]
@@ -54,67 +87,43 @@ $ ls
     }
 
     fn example_directory() -> Directory {
-        let a = Directory {
-            name: "a".to_string(),
-            sub_directories: vec![Directory {
-                name: "e".to_string(),
-                files: vec![File {
-                    name: "i".to_string(),
-                    size: 584,
-                }],
-                sub_directories: vec![],
-            }],
-            files: vec![
-                File {
-                    name: "f".to_string(),
-                    size: 29116,
-                },
-                File {
-                    name: "g".to_string(),
-                    size: 2557,
-                },
-                File {
-                    name: "h.lst".to_string(),
-                    size: 62596,
-                },
-            ],
+        let root = Directory {
+            name: "/".to_string(),
+            sub_directories: HashMap::new(),
+            files: HashMap::from([
+                ("b.txt".to_string(), 14848514),
+                ("c.txt".to_string(), 8504156)
+            ]),
+            parent: Vec::new()
         };
+        let mut a = Directory {
+            name: "a".to_string(),
+            files: HashMap::from([
+                ("f".to_string(), 29116),
+                ("g".to_string(), 2557),
+                ("h.lst".to_string(), 62596)
+            ]),
+            sub_directories: HashMap::from([
+                ("a".to_string(), Directory {
+                    name: "e".to_string(),
+                    files: HashMap::from([("i".to_string(), 584)]),
+                    sub_directories: HashMap::new(),
+                    parent: todo!()
+            })]),
+            parent: vec!(root),
+        };
+        a.sub_directories["a".to_string()].parent = a;
         let d = Directory {
             name: "d".to_string(),
-            sub_directories: vec![],
-            files: vec![
-                File {
-                    name: "j".to_string(),
-                    size: 4060174,
-                },
-                File {
-                    name: "d.log".to_string(),
-                    size: 8033020,
-                },
-                File {
-                    name: "d.ext".to_string(),
-                    size: 5626152,
-                },
-                File {
-                    name: "k".to_string(),
-                    size: 7214296,
-                },
-            ],
+            sub_directories: HashMap::new(),
+            files: HashMap::from([
+                ("j".to_string(), 4060174),
+                ("d.log".to_string(), 8033020),
+                ("d.ext".to_string(), 5626152),
+                ("k".to_string(), 7214296)
+            ]),
+            parent: vec!(root)
         };
-        Directory {
-            name: "/".to_string(),
-            sub_directories: vec![a, d],
-            files: vec![
-                File {
-                    name: "b.txt".to_string(),
-                    size: 14848514,
-                },
-                File {
-                    name: "c.txt".to_string(),
-                    size: 8504156,
-                },
-            ],
-        }
     }
 
     #[test]
