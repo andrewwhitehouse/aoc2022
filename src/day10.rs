a1e768ef9cb28a6829582b9180cbc690bfd4d1c4
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-12-20 14:29:15 +0000

    Initial Day 10 test passing

--- src/day10.rs
+++ src/day10.rs
@@ -11,7 +11,33 @@ pub struct Instruction {
 }
 
 pub fn execute(instructions: Vec<Instruction>, sample_at_cycle: Vec<u32>) -> Vec<i32> {
-    Vec::new()
+    let mut x = 1;
+    let mut cycle = 1;
+    let mut sample_index = 0;
+    let mut sample_values: Vec<i32> = Vec::new();
+    for instruction in instructions {
+        let mut new_x;
+        let cycles_for_instruction = match instruction.instruction_type {
+            InstructionType::NOOP => {
+                new_x = x;
+                1
+            },
+            InstructionType::ADDX => {
+                new_x = x + instruction.parmeter.unwrap();
+                2
+            }
+        };
+        for i in 0..cycles_for_instruction {
+            if cycle+i == sample_at_cycle[sample_index] {
+                sample_values.push(x);
+                sample_index += 1;
+            }
+        }
+        x = new_x;
+        cycle += cycles_for_instruction;
+    }
+    sample_values.push(x);
+    sample_values
 }
 
 #[cfg(test)]
@@ -25,7 +51,7 @@ mod day10_tests {
             Instruction{instruction_type: InstructionType::ADDX, parmeter: Some(3)},
             Instruction{instruction_type: InstructionType::ADDX, parmeter: Some(-5)},
         );
-        let result = execute(instructions, (1..=5).collect());
+        let result = execute(instructions, (1..=6).collect());
         assert_eq!(result, vec!(1, 1, 1, 4, 4, -1));
     }
 }
